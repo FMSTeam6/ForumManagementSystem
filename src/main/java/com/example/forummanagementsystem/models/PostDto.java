daef2ce137d6a9b966972006b80a26dce1f23f14
LPtanev <lyudmil.p.tanev@gmail.com>
2024-01-23 21:05:38 +0200

    Push with postRepository and postDto, filterOptions is empty

---
+++ src/main/java/com/example/forummanagementsystem/models/PostDto.java
@@ -0,0 +1,44 @@
+package com.example.forummanagementsystem.models;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.sql.Timestamp;
+
+public class PostDto {
+    @NotNull(message = "Please choose a meaningfully title")
+    @Size(min = 16, max = 64, message = "Title should be between 16 and 64 symbols")
+    private String title;
+    @NotNull(message = "Content can't be empty")
+    @Size(min = 32, max = 8192, message = "Content can be between 32 and 8192 symbols")
+    private String content;
+    // TODO
+    private Timestamp timestampCreated;
+
+    public PostDto() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getTimestampCreated() {
+        return timestampCreated;
+    }
+
+    public void setTimestampCreated(Timestamp timestampCreated) {
+        this.timestampCreated = timestampCreated;
+    }
+}
---
+++ src/main/java/com/example/forummanagementsystem/repositories/PostRepository.java
@@ -0,0 +1,31 @@
+package com.example.forummanagementsystem.repositories;
+
+import com.example.forummanagementsystem.models.FilterOptions;
+import com.example.forummanagementsystem.models.Post;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface PostRepository {
+    List<Post> get(FilterOptions filterOptions);
+
+    Post getPostById(int id);
+
+    Post getPostByName(String title);
+
+    Post getPostByLikes(int likes);
+
+    Post getPostByDislikes(int dislikes);
+
+    Post getPostByTimeStamp(Timestamp timestampCreated);
+
+    void create(Post post);
+
+    Post updatePost(Post post);
+
+    void deletePost(int id);
+
+    int likePost(Post post);
+
+    int dislikePost(Post post);
+}
